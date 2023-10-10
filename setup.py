2b1b7b0e7d54e864fb7b4ef0e888c81b04175e29
jeff0102 <jefferson.jvega@gmail.com>
2023-10-10 17:28:05 -0300

    setup added

---
+++ setup.py
@@ -0,0 +1,47 @@
+import os
+from setuptools import setup, find_packages
+
+with open(
+    os.path.join(os.path.dirname(__file__), "requirements.txt"), "r"
+) as fh:
+    requirements = fh.readlines()
+
+NAME = "b2u-connector-python"
+DESCRIPTION = (
+    "This is a lightweight library that works as a connector to b2u bank public API."
+)
+AUTHOR = "jeff0102"
+URL = "https://github.com/jeff0102/b2u-conector-python"
+VERSION = None
+
+about = {}
+
+root = os.path.abspath(os.path.dirname(__file__))
+
+if not VERSION:
+    with open(os.path.join(root, "b2u", "__version__.py")) as f:
+        exec(f.read(), about)
+else:
+    about["__version__"] = VERSION
+
+setup(
+    name=NAME,
+    version=about["__version__"],
+    license="MIT",
+    description=DESCRIPTION,
+    AUTHOR=AUTHOR,
+    url=URL,
+    keywords=["b2u", "Public API"],
+    install_requires=[req for req in requirements],
+    packages=find_packages(exclude=("tests",)),
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Intended Audience :: Financial and Insurance Industry",
+        "License :: MIT License",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+    python_requires=">=3.8",
+)
